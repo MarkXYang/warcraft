04698d5890461dd526ecd7c68646b607dacf6672
Ranxy <huran@outlook.com>
2014-07-22 21:45:06 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  
+#include <iomanip>  
+#include <string>  
+using namespace std;
+
+class warrior
+{
+  int number;
+  int life;
+  warrior(int n,int l):number(n),life(l)
+  {
+  }
+};
