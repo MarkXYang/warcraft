7643caf2c6e892ecfeaa1ed2af30f4170b954e15
Ranxy <huran@outlook.com>
2014-08-20 21:56:39 +0800

    fix bugs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdio>
 #include <string>
 class Command;
 class Warrior;
@@ -25,7 +27,7 @@ public:
 		}
 
 	}
-	Weapons(int numbers):attr(0)
+	Weapons(int numbers) :attr(0)
 	{
 		getWeaopn(numbers);
 	}
@@ -34,25 +36,28 @@ public:
 		return name;
 	}
 };
+//抽象基类WARRIOR
 class Warrior
 {
 protected:
 	int life;
-	static int number;
+	int number;
 	std::string names;
 	Command *pa;
 public:
-	Warrior(int life_,Command &p_);
-	void printCommands(int times);
+	Warrior(int life_, Command &p_);
+	virtual ~Warrior(){}
+	void prints(int times);
+	virtual void printCommand(int times) = 0;                          //纯虚函数
 	friend class Command;
-}; 
+};
 class Dragon : public Warrior
 {
 	Weapons weapon;
 	double morale;
 public:
 	void getMorale();
-	Dragon(int life_,Command &p):Warrior(life_,p),weapon(number)
+	Dragon(int life_, Command &p) :Warrior(life_, p), weapon(number)
 	{
 		getMorale();
 		names = "dragon";
@@ -65,7 +70,7 @@ class Ninja : public Warrior
 	Weapons weapon1;
 	Weapons weapon2;
 public:
-	Ninja(int life_,Command &p):Warrior(life_,p),weapon1(number %3),weapon2((number + 1) % 3)
+	Ninja(int life_, Command &p) :Warrior(life_, p), weapon1(number % 3), weapon2((number + 1) % 3)
 	{
 		names = "Ninja";
 	}
@@ -76,7 +81,7 @@ class Iceman : public Warrior
 {
 	Weapons weapon;
 public:
-	Iceman(int life_,Command &p):Warrior(life_,p),weapon(number)
+	Iceman(int life_, Command &p) :Warrior(life_, p), weapon(number)
 	{
 		names = "iceman";
 	}
@@ -88,7 +93,7 @@ class Lion : public Warrior
 	double loylaty;
 public:
 	void getLoylaty();
-	Lion(int life_,Command &p):Warrior(life_,p)
+	Lion(int life_, Command &p) :Warrior(life_, p)
 	{
 		getLoylaty();
 		names = "Lion";
@@ -96,21 +101,21 @@ public:
 	void printCommand(int times)
 	{
 		getLoylaty();
-		Warrior::printCommands(times);
-		std::cout<<"it's loylaty is "<<loylaty<<std::endl;
+		Warrior::prints(times);
+		std::cout << "it's loylaty is " << loylaty << std::endl;
 	}
 
 };
 class Wolf : public Warrior
 {
 public:
-	Wolf(int life_,Command &p):Warrior(life_,p)
+	Wolf(int life_, Command &p) :Warrior(life_, p)
 	{
 		names = "wolf";
 	}
 	void printCommand(int times)
 	{
-		Warrior::printCommands(times);
+		Warrior::prints(times);
 	}
 
 };
@@ -130,11 +135,11 @@ private:
 	Iceman iceman;
 	Lion lion;
 	Wolf wolf;
-	int bornjudge(int i,int time);
+	int bornjudge(int i, int time);
 	Warrior *warri[5];
-public:	
+public:
 
-	Command(std::string headName,int life,int lifeDragon,int lifeNinjia,int lifeIceman,int lifeLion,int lifeWolf);
+	Command(std::string headName, int life, int lifeDragon, int lifeNinjia, int lifeIceman, int lifeLion, int lifeWolf);
 	int bornwarrior(int time);                          //use bornjudge  and return is stoped.
 	friend class Warrior;
 	friend class Dragon;
@@ -143,47 +148,48 @@ public:
 	friend class Lion;
 	friend class Wolf;
 };
-Warrior::Warrior(int lifes,Command &p_):life(lifes),pa(&p_)
+Warrior::Warrior(int lifes, Command &p_) :life(lifes), pa(&p_)
 {
 	number = pa->warriorNumber;
 }
-void Warrior::printCommands(int times)
-	{
-		std::cout<<times<<' '<<pa->headName<<' '<<names<<' '<<pa->warriorNumber<<" born with strength "<<
-			life<<','<<*(pa->switchWarrior)<<' '<<names<<" in "<<pa->headName<<" headquarter"<<std::endl;
-	}
-void Ninja::printCommand(int times)
-	{
-		weapon1.getWeaopn(pa->warriorNumber %3);
-		weapon2.getWeaopn((pa->warriorNumber+1) %3);
-		Warrior::printCommands(times);
-		std::cout<<"it has a "<<weapon1.getName()<<" and a "<<weapon2.getName()<<std::endl;
-	}
-void Dragon::printCommand(int times)
-	{
-		weapon.getWeaopn(pa->warriorNumber);
-		getMorale();
-		Warrior::printCommands(times);
-		std::cout<<"it has a "<<weapon.getName()<<", and it's morale is "<<morale<<std::endl;
-	}
-void Iceman::printCommand(int times)
-	{
-		weapon.getWeaopn(pa->warriorNumber);
-		Warrior::printCommands(times);
-		std::cout<<"it has a "<<weapon.getName()<<std::endl;
-	}
-void Lion::getLoylaty()
-	{
-		loylaty = pa->life;
-	}
-void Dragon::getMorale()
-	{
-		morale = pa->life / (double)life;
-	}
-Command::Command(std::string headName_,int life_,int lifeDragon,int lifeNinjia,int lifeIceman,int lifeLion,int lifeWolf):
-	headName(headName_),life(life_),dragon(lifeDragon,*this),ninja(lifeNinjia,*this),
-	iceman(lifeIceman,*this),lion(lifeLion,*this),wolf(lifeWolf,*this),numberWarrior(),warriorNumber(0)
+void Warrior::prints(int times)
 {
+	std::cout << std::setfill('0') << std::setw(3) << times << ' ' << pa->headName << ' ' << names << ' ' << pa->warriorNumber << " born with strength " <<
+		life << ',' << *(pa->switchWarrior) << ' ' << names << " in " << pa->headName << " headquarter" << std::endl;
+}
+void Ninja::printCommand(int times)
+{
+	weapon1.getWeaopn(pa->warriorNumber % 3);
+	weapon2.getWeaopn((pa->warriorNumber + 1) % 3);
+	Warrior::prints(times);
+	std::cout << "It has a " << weapon1.getName() << " and a " << weapon2.getName() << std::endl;
+}
+void Dragon::printCommand(int times)
+{
+	weapon.getWeaopn(pa->warriorNumber);
+	getMorale();
+	Warrior::prints(times);
+	std::cout << "It has a " << weapon.getName() << ", and it's morale is " << std::setprecision(3) << morale << std::endl;
+}
+void Iceman::printCommand(int times)
+{
+	weapon.getWeaopn(pa->warriorNumber % 3);
+	Warrior::prints(times);
+	std::cout << "It has a " << weapon.getName() << std::endl;
+}
+void Lion::getLoylaty()
+{
+	loylaty = pa->life;
+}
+void Dragon::getMorale()
+{
+	morale = pa->life / (double)life;
+}
+Command::Command(std::string headName_, int life_, int lifeDragon, int lifeNinjia, int lifeIceman, int lifeLion, int lifeWolf) :
+headName(headName_), life(life_), dragon(lifeDragon, *this), ninja(lifeNinjia, *this),
+iceman(lifeIceman, *this), lion(lifeLion, *this), wolf(lifeWolf, *this), warriorNumber(0)
+{
+
 	warri[0] = &dragon;
 	warri[1] = &ninja;
 	warri[2] = &iceman;
@@ -191,60 +197,51 @@ Command::Command(std::string headName_,int life_,int lifeDragon,int lifeNinjia,i
 	warri[4] = &wolf;
 	stoped = false;
 	nexti = 0;
+	for (int j = 0; j < 5; j++)
+	{
+		numberWarrior[j] = 0;
+	}
 
 }
-int Command::bornjudge(int i,int time)
+int Command::bornjudge(int i, int time)
 {
-	void *pwarrior[5] = {&dragon,&ninja,&iceman,&lion,&wolf };
-	int s1[]={2,3,4,1,0};
-	int s2[]={3,0,1,2,4};
+	int s1[] = { 2, 3, 4, 1, 0 };
+	int s2[] = { 3, 0, 1, 2, 4 };
 	int searchTimes = 0;
-	if(headName =="red")
+	if (headName == "red")
 	{
 		order = s1;
 	}
-	else if(headName =="blue")
+	else if (headName == "blue")
 	{
 		order = s2;
 	}
 	switch (order[i])
 	{
-	case 0: 
-		switchWarrior = &numberWarrior[0]; 
+	case 0:
+		switchWarrior = &numberWarrior[0];
 		break;
 	case 1:
-		switchWarrior = &numberWarrior[1]; 
+		switchWarrior = &numberWarrior[1];
 		break;
 	case 2:
-		switchWarrior = &numberWarrior[2]; 
+		switchWarrior = &numberWarrior[2];
 		break;
 	case 3:
-		switchWarrior =	&numberWarrior[3]; 
+		switchWarrior = &numberWarrior[3];
 		break;
-	case 4: 
-		switchWarrior = &numberWarrior[4]; 
+	case 4:
+		switchWarrior = &numberWarrior[4];
 		break;
 	}
-	while( searchTimes <= 5 )
+	while (searchTimes <= 5)
 	{
-		if((*warri[order[i]]).life<=life)
+		if ((*warri[order[i]]).life <= life)
 		{
-			life =life-(*warri[order[i]]).life;
+			life = life - (*warri[order[i]]).life;
 			warriorNumber++;
 			(*switchWarrior)++;
-			switch (order[i])
-			{
-			case 0:
-				dragon.printCommand(time);break;
-			case 1:
-				ninja.printCommand(time);break;
-			case 2:
-				iceman.printCommand(time);break;
-			case 3:
-				lion.printCommand(time);break;
-			case 4:
-				wolf.printCommand(time);break;
-			}
+			warri[order[i]]->printCommand(time);
 			i++;
 			i = i % 5;
 			break;
@@ -254,22 +251,22 @@ int Command::bornjudge(int i,int time)
 			i++;
 			i = i % 5;
 			searchTimes++;
-			if(searchTimes == 5)
+			if (searchTimes == 5)
 			{
 				stoped = true;
 				break;
 			}
 		}
 	}
-	if(stoped)
+	if (stoped)
 	{
-		std::cout<<time<<' '<<headName<<" headquarter stops making warriors"<<std::endl;
+		std::cout << std::setfill('0') << std::setw(3) << time << ' ' << headName << " headquarter stops making warriors" << std::endl;
 	}
 	return i;
 }
 int Command::bornwarrior(int time)
 {
-	if(stoped)
+	if (stoped)
 	{
 		return stoped;
 	}
@@ -279,35 +276,35 @@ int Command::bornwarrior(int time)
 		return stoped;
 	}
 }
-int Warrior::number = 0;
 int main()
 {
 	int how;
-	std::cin>>how;
-	for(int i = 0; i < how; i++)
+	std::cin >> how;
+	for (int i = 0; i < how; i++)
 	{
+		std::cout << "case:" << i + 1 << std::endl;
 		int life;
-		std::cin>>life;
-		int s[5];
-		for(int j = 0; j < 5; j++)
+		std::cin >> life;
+		int s[5] = {};
+		for (int j = 0; j < 5; j++)
 		{
-			std::cin>>s[j];
+			std::cin >> s[j];
 		}
-		std::cout<<"case:"<<i+1<<std::endl;
+
 		int times = 0;
 		int r = 0, b = 0;
 		Command red("red", life, s[0], s[1], s[2], s[3], s[4]);
 		Command blue("blue", life, s[0], s[1], s[2], s[3], s[4]);
-		while(1)
+		while (1)
 		{
 			r = red.bornwarrior(times);
 			b = blue.bornwarrior(times);
 			times++;
-			if(r && b)
+			if (r && b)
 			{
 				break;
 			}
-		
+
 		}
 	}
 	return 0;
